b35a5bf8e0cc82323f57790fdcf6dbf184be587b
olaszakos <olaszakos@gmail.com>
2021-10-10 11:13:55 +0200

    install process fix; install instructions in readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # candao
 
 A hackathon project by Ákos, Artem, and Fulco
+
+## Install
+
+1. Install DFX
+   ```
+   $ sh -ci "$(curl -fsSL https://smartcontracts.org/install.sh)"
+   ```
+1. Run init script
+
+   ```
+   $ sh init.sh
+   ```
+
+   This will install Internet Identity in the local replica.
+
+1. Open front-end at http://localhost:3000
--- init.sh
+++ init.sh
@@ -3,14 +3,8 @@ git submodule update --init --recursive
 cd internet-identity
 npm install
 II_ENV=development dfx deploy --no-wallet --argument '(null)'
-II_CANISTER_ID=$(dfx canister --no-wallet id internet_identity)
-cd ..
-
-cat <<EOF >| local_canisters.json
-   {
-    "II_LOCAL_UI_CANISTER_ID": "$II_CANISTER_ID"
-   }
-EOF
+cd ../src/candao_assets
 npm install
+cd ../..
 dfx deploy
 npm start
