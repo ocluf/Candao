14253bcaf850ce5b381bec9b63e885c89279916b
ocluf <fulco.taen@gmail.com>
2021-10-12 00:36:26 +0200

    Added functions which allow you to create and vote on Member proposals.

--- src/candao/src/main.rs
+++ src/candao/src/main.rs
@@ -8,7 +8,7 @@ use std::convert::TryInto;
 #[derive(Clone, CandidType, Deserialize)]
 enum ProposalType {
     AddMember(Member),
-    RemoveMember,
+    RemoveMember(Principal),
     CreateCanister,
     InstallCanister,
     DeleteCanister,
@@ -20,8 +20,15 @@ enum ProposalType {
 #[derive(Clone, CandidType, Deserialize)]
 enum ProposalStatus {
     InProgress,
-    Accepted,
+    Executed,
+    Failed,
     Rejected,
+}
+
+#[derive(Clone, CandidType, Deserialize)]
+enum Vote {
+    Yes,
+    No,
 }
 
 #[derive(Clone, CandidType, Deserialize)]
@@ -92,9 +99,48 @@ fn create_proposal(proposal_type: ProposalType) -> bool {
         no_votes: vec![],
     };
     STATE.with(|s| s.proposals.borrow_mut().push(proposal));
+    STATE.with(|s| check_votes(s.proposals.borrow_mut().last_mut().unwrap()));
 
     //TODO replace with proper response message enum
     return true;
+}
+
+#[update]
+fn vote(proposal_id: u64, ballot: Vote) -> bool {
+    let voter = caller();
+    if !is_member(voter) {
+        return false;
+    }
+    STATE.with(|m| {
+        match m
+            .proposals
+            .borrow_mut()
+            .iter_mut()
+            .find(|p| p.proposal_id == proposal_id)
+        {
+            Some(proposal) => {
+                if !proposal.no_votes.contains(&voter) && !proposal.yes_votes.contains(&voter) {
+                    match ballot {
+                        Vote::Yes => {
+                            proposal.yes_votes.push(voter);
+                        }
+                        Vote::No => {
+                            proposal.no_votes.push(voter);
+                        }
+                    }
+                    check_votes(proposal); //should this be a seperate update call?
+                    return true;
+                } else {
+                    // already voted
+                    return false;
+                }
+            }
+            None => {
+                // no proposal with that id
+                return false;
+            }
+        }
+    })
 }
 
 #[query]
@@ -111,6 +157,48 @@ fn get_proposals() -> Vec<Proposal> {
     })
 }
 
+fn check_votes(proposal: &mut Proposal) {
+    if !matches!(proposal.proposal_status, ProposalStatus::InProgress) {
+        return;
+    }
+    let total_members = STATE.with(|s| s.members.borrow().len());
+    let majority = total_members / 2 + 1;
+    let nr_of_yes = proposal.yes_votes.len();
+    let nr_of_no = proposal.no_votes.len();
+    if nr_of_yes > nr_of_no && nr_of_yes >= majority {
+        let result = execute(&proposal);
+        match result {
+            Ok(_) => proposal.proposal_status = ProposalStatus::Executed,
+            Err(_) => proposal.proposal_status = ProposalStatus::Failed,
+        }
+    } else if nr_of_no > nr_of_yes && nr_of_no >= majority {
+        proposal.proposal_status = ProposalStatus::Rejected;
+    } else if nr_of_no == nr_of_yes && nr_of_no + nr_of_yes == total_members {
+        proposal.proposal_status = ProposalStatus::Rejected;
+    }
+}
+
+fn execute(proposal: &Proposal) -> Result<(), ()> {
+    match &proposal.proposal_type {
+        ProposalType::AddMember(member) => {
+            STATE.with(|s| s.members.borrow_mut().push(member.clone()));
+            return Ok(());
+        }
+        ProposalType::RemoveMember(principal) => STATE.with(|s| {
+            s.members
+                .borrow_mut()
+                .retain(|m| m.principal_id != *principal);
+            return Ok(());
+        }),
+        ProposalType::CreateCanister => todo!(),
+        ProposalType::InstallCanister => todo!(),
+        ProposalType::DeleteCanister => todo!(),
+        ProposalType::StartCanister => todo!(),
+        ProposalType::StopCanister => todo!(),
+        ProposalType::UpdateCanisterSettings => todo!(),
+    }
+}
+
 fn is_member(principal: Principal) -> bool {
     STATE.with(|s| {
         s.members
@@ -125,5 +213,4 @@ fn add_member(new_member: Member) {
     STATE.with(|s| s.members.borrow_mut().push(new_member))
 }
 
-fn emptssy() {}
 fn main() {}
