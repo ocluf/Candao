9828fa50438b0c8b4aedbd98148c4da7e2adc924
olaszakos <olaszakos@gmail.com>
2021-10-16 08:49:35 +0200

    UI update (#6)
    * update: home page UI; purge script;
    
    * update: mobile nav fix; claim page wip;

---
+++ src/candao_assets/components/Nav.tsx
@@ -0,0 +1,128 @@
+import { Disclosure } from "@headlessui/react";
+import classNames from "classnames";
+import Link from "next/link";
+import React from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { useDaoInfo } from "../hooks/useDaoInfo";
+import { LoginState, useAuth } from "./AuthProvider";
+
+const user = {
+  name: "Tom Cook",
+  email: "tom@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+
+const userNavigation = [
+  // { name: "Your Profile", href: "#" },
+  // { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+
+const navigation = [
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Members", href: "#", current: false },
+  { name: "Canisters", href: "#", current: false },
+  { name: "Proposals", href: "#", current: false },
+];
+
+export const Nav: React.FC<{ showMenu: boolean }> = ({ showMenu = true }) => {
+  const { authState, logout, login } = useAuth();
+
+  const { daoInfo, daoInfoError, daoInfoLoading } = useDaoInfo();
+
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }: { open: boolean }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16">
+              <div className="flex items-center">
+                <Link passHref href="/">
+                  <a className="flex-shrink-0 flex items-center">
+                    <img className="h-8 w-8" src="/images/logo.svg" alt="" />
+                    <span className="pl-2 text-white font-semibold">
+                      CanDAO
+                    </span>
+                  </a>
+                </Link>
+                {showMenu && (
+                  <div className="hidden md:block">
+                    <div className="ml-10 flex items-baseline space-x-4">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "px-3 py-2 rounded-md text-sm font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-4 flex items-center md:ml-6">
+                  <button
+                    type="button"
+                    className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-200 bg-transparent hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={() =>
+                      authState === LoginState.LoggedIn ? logout() : login()
+                    }
+                  >
+                    {authState === LoginState.LoggedIn ? "Log out" : "Log in"}
+                  </button>
+                </div>
+              </div>
+              <div className="-mr-2 flex md:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <FiX className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <FiMenu className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+          <Disclosure.Panel className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
+              <button
+                className={
+                  "w-full text-left text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                }
+                onClick={logout}
+              >
+                Log out
+              </button>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+};
---
+++ src/candao_assets/hooks/useDaoMembers.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "react-query";
+import { useActor } from "../components/ActorProvider";
+import { useAuth } from "../components/AuthProvider";
+
+export function useDaoMembers() {
+  const { actor } = useActor();
+  const { authClient } = useAuth();
+  const { isLoading, error, data } = useQuery("daoMembers", () =>
+    actor.get_members()
+  );
+
+  return {
+    daoMembersLoading: isLoading,
+    daoMembersError: error,
+    daoMembers: data,
+    me:
+      authClient &&
+      data &&
+      data.find(
+        (m) =>
+          m.principal_id.toString() ===
+          authClient.getIdentity().getPrincipal().toString()
+      ),
+  };
+}
