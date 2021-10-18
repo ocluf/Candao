4d97eea3ad79952f403bad423f5ba00777756da3
ocluf <fulco.taen@gmail.com>
2021-10-18 18:11:12 +0200

    You can now update member info on the specfic member page and also create a remove proposal

--- src/candao_assets/pages/members/[principal]/index.tsx
+++ src/candao_assets/pages/members/[principal]/index.tsx
@@ -4,22 +4,19 @@ import PageHeading from "../../../components/PageHeading";
 import MemberForm from "../../../components/MemberForm";
 import { useDaoMembers } from "../../../hooks/useDaoMembers";
 import { useRouter } from "next/dist/client/router";
-import { Disclosure } from "@headlessui/react";
+import RemoveMemberModal from "../../../components/RemoveMemberModal";
 import { Member } from "../../../declarations/candao/candao.did";
+import { useState } from "react";
 
 const Members: NextPage = () => {
   const router = useRouter();
   const urlPrincipal = router.query.principal;
   const { daoMembers, daoMembersError, daoMembersLoading } = useDaoMembers();
+  const [open, setOpen] = useState(false);
+  console.log("open in memberPage: " + open);
   const member: Member | undefined = daoMembers?.find(
     (members) => members.principal_id.toString() == urlPrincipal
   );
-
-  const createRemoveProposal = () => {
-    if (member) {
-      //TODO create remove proposal
-    }
-  };
 
   if (daoMembersLoading) {
     return (
@@ -44,23 +41,34 @@ const Members: NextPage = () => {
     );
   } else {
     return (
-      <div className="h-screen bg-gray-100">
-        <Nav current="Members"></Nav>
-        <PageHeading
-          crumbs={["Dashboard", "Members", "Member profile"]}
-          pageTitle="Member"
-          className="bg-transparent"
-        >
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 mr-8 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            onClick={() => createRemoveProposal()}
+      <>
+        <RemoveMemberModal
+          open={open}
+          setOpen={setOpen}
+          member={member}
+        ></RemoveMemberModal>
+        ;
+        <div className="h-screen bg-gray-100">
+          <Nav current="Members"></Nav>
+          <PageHeading
+            crumbs={["Dashboard", "Members", "Member profile"]}
+            pageTitle="Member"
+            className="bg-transparent"
           >
-            Remove
-          </button>
-        </PageHeading>
-        <MemberForm member={member}></MemberForm>
-      </div>
+            <button
+              type="button"
+              className="inline-flex items-center px-4 py-2 mr-8 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={() => {
+                console.log("click");
+                setOpen(true);
+              }}
+            >
+              Remove
+            </button>
+          </PageHeading>
+          <MemberForm member={member}></MemberForm>
+        </div>
+      </>
     );
   }
 };
