3f498be7f949a96b95b36c8ffa19939a9ff75414
ocluf <fulco.taen@gmail.com>
2021-10-29 19:07:57 +0200

    some extra minor styling

--- src/candao_assets/pages/proposals.tsx
+++ src/candao_assets/pages/proposals.tsx
@@ -294,8 +294,8 @@ const ProposalCard: React.FC<{
   };
 
   return (
-    <Card className="flex">
-      <div className="flex flex-1 flex-col space-y-2">
+    <Card className="flex shadow sm:rounded-lg">
+      <div className="flex flex-1 flex-col space-y-2 truncate">
         <h1 className="text-base leading-6 font-bold">
           {getProposalText()[0]}
         </h1>
@@ -509,7 +509,7 @@ const Proposals: NextPage = () => {
           <select
             id="current-tab"
             name="current-tab"
-            className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            className="block w-full pl-3 pr-10 py-2 text-base border-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
             defaultValue={currentTab}
           >
             {tabs.map((tab) => (
